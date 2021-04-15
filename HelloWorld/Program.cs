035de3908daa5cb35d6ec8789514388cec155839
Gabriel Mendes <gabrielrfmendes@gmail.com>
2021-04-14 21:27:41 -0300

    Hello, world!

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿// diretiva using faz referência ao um namespace
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        // static void declara um método estático
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
