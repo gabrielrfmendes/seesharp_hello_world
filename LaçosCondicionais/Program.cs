75bf88d6d4ee28453c5c95e8e0ab6e71649d93ed
Gabriel Mendes <gabrielrfmendes@gmail.com>
2021-04-18 20:50:34 -0300

    Estrutura condicional

---
+++ LaçosCondicionais/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LaçosCondicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Laço condicional if
+            var test = true;
+            if (!test)
+            {
+                Console.WriteLine("Teste é falso");
+            }
+            else
+            {
+                Console.WriteLine("Teste é verdadeiro");
+            }
+
+            // Switch
+        }
+    }
+}
